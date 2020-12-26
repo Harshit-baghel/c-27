4cfbd255a4cebe90b4fd027a4ace1604fd0b6d09
Harshit-baghel <73468226+Harshit-baghel@users.noreply.github.com>
2020-12-26 14:34:29 +0530

    chain

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,8 +34,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6=new Log(100,100,100,PI);
     bird = new Bird(100,100);
+    chain=new Chain(bird.body,log6.body)
+    abc=new Chain(platform.body,pig1.body);
 
 }
 
@@ -58,6 +61,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    chain.display();
+    abc.display();
 
     bird.display();
     platform.display();
